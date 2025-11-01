c68537b9d0f7ab0a38b5c80ae46de1f1e301b222
Max Raymond <maxr9090@gmail.com>
2025-10-31 21:08:49 -0400

    feat: add recognizing faces and displaying the faces with border box and label

--- detector.py
+++ detector.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import face_recognition
 import pickle
+from collections import Counter
+from PIL import Image, ImageDraw, ImageFont
 
 DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
 
@@ -8,7 +10,52 @@ Path("training").mkdir(exist_ok=True)
 Path("output").mkdir(exist_ok=True)
 Path("validation").mkdir(exist_ok=True)
 
-def encode_known_faces(model: str = "hog", encodings_locations: Path = DEFAULT_ENCODINGS_PATH) -> None:
+BOUNDING_BOX_COLOR = (0,0,255)
+TEXT_COLOR = (255,255,255)
+
+# Helper functions
+def _recognize_face(unknown_encoding, loaded_encodings):
+    boolean_matches = face_recognition.compare_faces(
+        loaded_encodings["encodings"], unknown_encoding
+    )
+    votes = Counter(
+        name
+        for match, name in zip(boolean_matches, loaded_encodings["names"])
+        if match
+    )
+    if votes:
+        return votes.most_common(1)[0][0]
+
+def _display_face(draw, bounding_box, name):
+    top, right, bottom, left = bounding_box
+    draw.rectangle(((left, top), (right, bottom)), outline=BOUNDING_BOX_COLOR, width=2)
+
+    try:
+        font = ImageFont.truetype("fonts/BebasNeue-Regular.ttf", size=30)
+    except:
+        print("Cannot set the font.")
+
+    text_left, text_top, text_right, text_bottom = draw.textbbox(
+        (left, bottom), name
+    )
+    draw.rectangle(
+        ((text_left, text_top), (text_right, text_bottom)),
+        fill=BOUNDING_BOX_COLOR,
+        outline=BOUNDING_BOX_COLOR
+    )
+    draw.text(
+        (left, bottom),
+        name,
+        fill=TEXT_COLOR,
+        font=font,
+        stroke_width=2,      
+        stroke_fill=(0, 0, 0) 
+    )
+
+def encode_known_faces(
+        model: str = "hog", 
+        encodings_locations: Path = DEFAULT_ENCODINGS_PATH
+) -> None:
     names = []
     encodings = []
     image_extensions = {".jpg", ".jpeg", ".png"}
@@ -30,7 +77,36 @@ def encode_known_faces(model: str = "hog", encodings_locations: Path = DEFAULT_E
     with encodings_locations.open(mode="wb") as f:
         pickle.dump(name_encodings, f)
 
-encode_known_faces()
+def recognize_faces(
+        image_location: str,
+        model: str = "hog",
+        encodings_location: Path = DEFAULT_ENCODINGS_PATH
+) -> None:
+    with encodings_location.open(mode="rb") as f:
+        loaded_encodings = pickle.load(f)
 
+    input_image = face_recognition.load_image_file(image_location)
 
-        
+    input_face_locations = face_recognition.face_locations(
+        input_image, model=model
+    )
+    input_face_encodings = face_recognition.face_encodings(
+        input_image, input_face_locations
+    )
+
+    pillow_image = Image.fromarray(input_image)
+    draw = ImageDraw.Draw(pillow_image)
+
+    for bounding_box, unknown_encoding in zip(
+        input_face_locations, input_face_encodings
+    ):
+        name = _recognize_face(unknown_encoding, loaded_encodings)
+        if not name:
+            name = "Unknown"
+        _display_face(draw, bounding_box, name)
+    
+    del draw
+    pillow_image.show()
+
+recognize_faces("unknown.jpg")
+recognize_faces("unknown2.jpg")
